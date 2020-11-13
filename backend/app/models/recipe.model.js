93e27bcccf35bc9945c2519c4bf4db735ecec3c3
Josh Fogus <josh.fogus@gmail.com>
2020-11-13 15:45:52 -0800

    Added routes for post, put, delete

--- backend/app/models/recipe.model.js
+++ backend/app/models/recipe.model.js
@@ -1,13 +1,14 @@
 const sql = require("./db.js");
 
 class Recipe {
-	constructor(recipeId, recipeUrl, name, description, meal, userId){
+	constructor(recipeId, recipeUrl, name, description, meal, userId, servingAmount){
 		this.recipeId = recipeId;
 		this.recipeUrl = recipeUrl;
 		this.name = name;
 		this.description = description;
 		this.meal = meal;
-		this.userId = userId
+		this.userId = userId;
+		this.servingAmount = servingAmount
 	}
 
 	static fromReqBody(reqBody){
@@ -17,7 +18,8 @@ class Recipe {
 			reqBody.name,
 			reqBody.description,
 			reqBody.meal,
-			reqBody.userId
+			reqBody.userId,
+			reqBody.servingAmount
 			);
 	}
 
@@ -28,38 +30,106 @@ class Recipe {
 			recipeDbDto.name,
 			recipeDbDto.description,
 			recipeDbDto.meal,
-			recipeDbDto.user_id
+			recipeDbDto.user_id,
+			recipeDbDto.serving_amount
 			);
 	}
 }
 
 class RecipeDbDto {
-	constructor(recipe_id, recipe_url, name, description, meal, user_id){
+	constructor(recipe_id, recipe_url, name, description, meal, user_id, serving_amount){
 		this.recipe_id = recipe_id;
 		this.recipe_url = recipe_url;
 		this.name = name;
 		this.description = description;
 		this.meal = meal;
 		this.user_id = user_id;
+		this.serving_amount = serving_amount;
 	}
 }
 
-
 Recipe.fetchAll = result => {
 	sql.query("SELECT * FROM recipes", (err, res) => {
-		if (err){
+		if (err) {
 			console.log("Error: ", err);
 			result(err, null);
 			return;
+		}
+
+		const recipeArray = [];
+		res.forEach(recipeDbDto => recipeArray.push(Recipe.fromRecipeDbDto(recipeDbDto)));
+		console.log("recipes: ", recipeArray);
+		result(null, res);
+	});
+}
+
+Recipe.fetchOne = (body, result) => {
+	sql.query(
+		"SELECT * FROM recipes WHERE recipes.recipe_id = ?;",
+		[parseInt(body.recipeId)],
+		(err, res) => {
+			if (err) {
+				console.log("Error: ", err);
+				result(err, null);
+				return;
 			}
 
-		recipeArray = [];
-		res.forEach(recipeDbDto => recipeArray.push(Recipe.fromRecipeDbDto(recipeDbDto)));
-		console.log("recipes: ",recipeArray);
-		result(null, res);
+			const recipe = Recipe.fromRecipeDbDto(res[0]);
+			console.log("recipe: ", recipe);
+			result(null, res);
+	});
+}
 
-	})	
+Recipe.addOne = (body, result) => {
+	sql.query(
+		"INSERT INTO recipes (user_id, recipe_url, name, description, meal, serving_amount) " +
+		"VALUES(?, ?, ?, ?, ?, ?);",
+		[
+			parseInt(body.userId),
+			body.recipeUrl,
+			body.name,
+			body.description,
+			body.meal,
+			parseInt(body.servingAmount)
+		],
+		(err, res) => {
+			if (err) {
+				console.log("Error: ", err);
+				result(err, null);
+				return;
+			}
 
+			console.log('rows affected: ', res.affectedRows);
+			result(null, res.affectedRows);
+	});
+}
+
+Recipe.updateOne = (body, params, result) => {
+	sql.query('UPDATE recipes SET ? WHERE ?;',
+		[body, params],
+		(err, res) => {
+			if (err) {
+				console.log("Error: ", err);
+				result(err, null);
+				return;
+			}
+
+			console.log("rows affected", res.affectedRows);
+			result(null, res.affectedRows);
+	});
+}
+
+Recipe.deleteOne = (body, result) => {
+	sql.query("DELETE FROM recipes WHERE recipe_id = ?;", [parseInt(body.recipeId)], (err, res) => {
+		if (err) {
+			console.log("Error: ", err);
+			result(err, null);
+			return;
+		}
+
+		console.log("rows affected", res.affectedRows);
+		result(null, res.affectedRows);
+	});
 }
 
 module.exports = Recipe
--- backend/app/routes/recipe.routes.js
+++ backend/app/routes/recipe.routes.js
@@ -1,6 +1,18 @@
 module.exports = app => {
 	const recipes = require("../controllers/recipe.controller")
 
-	// Retrieve all experts
+	// Retrieve all recipes
 	app.get("/recipes", recipes.findAll);
+
+	// Retrieve a recipe
+	app.get("/recipes/:recipeId", recipes.findOne);
+
+	// Add a recipe
+	app.post("/recipes", recipes.addOne);
+
+	// Update a recipe
+	app.put("/recipes/:recipe_id", recipes.updateOne);
+
+	// Delete a recipe
+	app.delete("/recipes/:recipeId", recipes.deleteOne);
 }
