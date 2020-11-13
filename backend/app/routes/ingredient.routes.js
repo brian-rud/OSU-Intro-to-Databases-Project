bcdc912e79b7db33e9f4bb98eecbb18781388065
Josh Fogus <josh.fogus@gmail.com>
2020-11-12 19:21:12 -0800

    Added routes for post, put, delete

--- backend/app/routes/ingredient.routes.js
+++ backend/app/routes/ingredient.routes.js
@@ -1,6 +1,15 @@
 module.exports = app => {
 	const ingredients = require("../controllers/ingredient.controller")
 
-	// Retrieve all experts
+	// Retrieve all ingredients
 	app.get("/ingredients", ingredients.findAll);
+
+	// Add an ingredient
+	app.post("/ingredients", ingredients.addOne);
+
+	// Change a single ingredient
+	app.put("/ingredients/:ingredientId", ingredients.updateOne);
+
+	// Delete a single ingredient
+	app.delete("/ingredients/:ingredientId", ingredients.deleteOne);
 }
