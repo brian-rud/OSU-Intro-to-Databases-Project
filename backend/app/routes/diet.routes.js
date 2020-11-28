07ed4eea15f28ba6a636ded68e7379fea9e0a15d
Josh Fogus <josh.fogus@gmail.com>
2020-11-28 08:42:19 -0800

    Updated diet routes

--- backend/app/routes/diet.routes.js
+++ backend/app/routes/diet.routes.js
@@ -1,7 +1,15 @@
 module.exports = app => {
 	const diets = require('../controllers/diet.controller.js');
 
+	// Retrieve all diets
 	app.get("/diets", diets.findAll);
 
-	app.post("/diets", diets.create)
+	// Add a diet
+	app.post("/diets", diets.create);
+
+	// Update a diet
+	app.put("/diets/:dietId", diets.updateOne);
+
+	// Delete a diet
+	app.delete("/diets/:dietId", diets.deleteOne);
 }
